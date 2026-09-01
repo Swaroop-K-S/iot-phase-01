26a6472d0fffc12e29bffa0c046d2d18104fd031
Apex 5 <110756546+Swaroop-K-S@users.noreply.github.com>
2026-09-01 12:03:12 +0530

    Char Frequency problem solved

---
+++ EmployeeProject/src/day6/day10/CharFrequency.java
@@ -0,0 +1,29 @@
+public class CharFrequency {
+    public static void main(String[] args) {
+        String message = "AAAABBCCD";
+        System.out.println("message " + message);
+        
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        
+        for (char ch : message.toCharArray()) {
+            if (ch == 'A') {
+                a++;
+            } else if (ch == 'B') {
+                b++;
+            } else if (ch == 'C') {
+                c++;
+            } else if (ch == 'D') {
+                d++;
+            }
+        }
+        
+        System.out.println("\nFrequency:");
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+        System.out.println("C = " + c);
+        System.out.println("D = " + d);
+    }
+}
